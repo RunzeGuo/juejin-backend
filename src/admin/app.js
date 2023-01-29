f5a90243f48f411096117285cf4683e26a646307
RunzeGuo <grz1614185525@gmail.com>
2023-01-29 12:32:12 +0800

    add chinese mode

---
+++ src/admin/app.js
@@ -0,0 +1,15 @@
+export default {
+  config: {
+    locales: ["zh-Hans"],
+    translations: {
+      "zh-Hans": {
+        "Auth.form.email.label": "yours@qq.com",
+        Users: "yours@qq.com",
+        City: "Chinese (Simplified) (zh-Hans)",
+        // 这里是刚刚在设置中新建的国际化语言版本的ID
+        Id: "3",
+      },
+    },
+  },
+  bootstrap() {},
+};
