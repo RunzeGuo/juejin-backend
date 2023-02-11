edcb624da9eff2a66391e28ba3b305021b0ccbc4
RunzeGuo <grz1614185525@gmail.com>
2023-02-12 01:51:23 +0800

    删了rank列表，只保留authors

--- src/api/author-rank-list/controllers/author-rank-list.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * author-rank-list controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::author-rank-list.author-rank-list');
--- src/api/author-rank-list/routes/author-rank-list.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * author-rank-list router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::author-rank-list.author-rank-list');
